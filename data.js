acab18fe93bb8523f7c47ea63f35cd7e4c119088
Marcin Chołaściński <marcincholascinski@macbook-pro-marcin.home>
2023-05-28 22:18:52 +0200

    Update js

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 fetch('./data.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-    document.getElementById('title-1').textContent = data[0].category;
+    document.getElementById('category-1').textContent = data[0].category;
+    document.getElementById('score-1').textContent = data[0].score;
+    document.getElementById('icon-1').src = "./assets/images/" + data[0].icon;
   })
   .catch(error => console.error('Error:', error));
