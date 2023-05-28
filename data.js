4b2bde84bbd3f7c135cb22679f387e4a032c3d7d
Marcin Chołaściński <marcincholascinski@macbook-pro-marcin.home>
2023-05-28 21:55:46 +0200

    Bugfix

--- data.js
+++ data.js
@@ -2,6 +2,6 @@ fetch('./data.json')
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    document.getElementById('title-1').textContent = data.[0].category;
+    document.getElementById('title-1').textContent = data.array[0].category;
   })
   .catch(error => console.error('Error:', error));
