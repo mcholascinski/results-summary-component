53cfe237352b4f859abae41dafd64cabf70b6688
Marcin Chołaściński <marcincholascinski@macbook-pro-marcin.home>
2023-05-28 22:58:43 +0200

    Add result to js

--- data.js
+++ data.js
@@ -1,10 +1,13 @@
 fetch('./data.json')
   .then(response => response.json())
   .then(data => {
-    for (let i = 0; i <= data.length; i++) {
+    let scoreSum = 0;
+    for (let i = 0; i < data.length; i++) {
       document.getElementById('category-' + (i + 1)).textContent = data[i].category;
       document.getElementById('score-' + (i + 1)).textContent = data[i].score;
       document.getElementById('icon-' + (i + 1)).src = data[i].icon;
+      scoreSum += data[i].score;
     }
+    document.getElementById('result').textContent = scoreSum;
   })
   .catch(error => console.error('Error:', error));
