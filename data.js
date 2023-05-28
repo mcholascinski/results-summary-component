fff9991c54b179b6153dbebfa9f8331217113676
Marcin Chołaściński <marcincholascinski@macbook-pro-marcin.home>
2023-05-28 22:32:47 +0200

    Bugfix

--- data.js
+++ data.js
@@ -2,9 +2,9 @@ fetch('./data.json')
   .then(response => response.json())
   .then(data => {
     for (let i = 0; i < 3; i++) {
-      document.getElementById('category-' + i + 1).textContent = data[i].category;
-      document.getElementById('score-' + i + 1).textContent = data[i].score;
-      document.getElementById('icon-' + i + 1).src = data[i].icon;
+      document.getElementById('category-' + (i + 1)).textContent = data[i].category;
+      document.getElementById('score-' + (i + 1)).textContent = data[i].score;
+      document.getElementById('icon-' + (i + 1)).src = data[i].icon;
     }
   })
   .catch(error => console.error('Error:', error));
